309aa01d4de406f700862a3eaa3397eaab47f6a9
Alastair Hogg <alastair_hogg2004@hotmail.com>
2025-04-10 18:11:34 +0100

    1234

--- terraform_for_AWS_cloud_2/test.tf
+++ terraform_for_AWS_cloud_2/test.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "MyFirstInstance" {
     instance_type   = "t2.micro"
 
     tags = {
-        Name = "demoinstance-${count.index}"
+        Name = "demoinstance12345-${count.index}"
     }
 }
