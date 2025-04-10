5055e7e563671d77627a26ef1dab5c4579a2eadc
Alastair Hogg <alastair_hogg2004@hotmail.com>
2025-04-10 18:42:02 +0100

    test deleted

--- terraform_for_AWS_cloud_2/test.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "MyFirstInstance" {
-    count           = 3
-    ami             = "ami-0b0ea68c435eb488d"
-    instance_type   = "t2.micro"
-
-    tags = {
-        Name = "demoinstance1b-${count.index}"
-    }
-}
